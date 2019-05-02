bed25ddb835be761e1b7e2053ea7eca3c5377b76
ew0k <jakebrown188@gmail.com>
2019-05-01 22:24:05 -0400

    Updated some parameters and comments

--- configure_exchange.ps1
+++ configure_exchange.ps1
@@ -53,6 +53,7 @@
     ###END EXAMPLE FILE###
 
     .PARAMETER ServerURI
+    #TODO: what is a proper fqdn?
     Specifies the URI of the exchange server. Used in function Connect-ExchangeServer as the -ConnectionUri argument. NOTE: format must be either one of the following
     ###FORMAT 1###
     http://<FQDN>/PowerShell
@@ -115,7 +116,7 @@
         [string]
         $PathToCSV,
 
-        [ValidatePattern('^https?://[\S+]+\.[\S+]+\.[\S]+/PowerShell$', Options='None')]
+        [ValidatePattern('^https?://[\S+]+/PowerShell$', Options='None')]
         [string]
         $ServerURI = "http://" + $env:COMPUTERNAME + "." + $env:USERDNSDOMAIN + "/PowerShell",
 
@@ -130,39 +131,139 @@
     )
 
 process {
+    <#
+        .DESCRIPTION
+        This function connects to the exchange server. This is necessary in order to run any exchange cmdlets later in the script.
+
+        .PARAMETER Password
+        Description: Specifies password to be used when connecting to the exchange server. NOTE: password must be of type System.Security.SecureString
+        Mandatory: Yes
+        Default Value: None
+
+        .PARAMETER ServerURI
+        Description: Specifies the URI of the exchange server. NOTE: format must be either one of the following
+        ###FORMAT 1###
+        http://<FQDN>/PowerShell
+        ###END FORMAT 1###
+
+        ###FORMAT 2###
+        https://<FQDN>/PowerShell
+        ###END FORMAT 2###
+
+        Mandatory: No
+        Default Value: http://$env:COMPUTERNAME.$env:USERDNSDOMAIN/PowerShell
+        NOTE: This default value is designed for running the script locally on the exchange server
+
+        .PARAMETER Username
+        Description: Specifies username used to connect to the exchange server.
+        Mandatory: No
+        Default Value: $env:USERNAME
+    #>
     function Connect-ExchangeServer {
         param (
-        [Parameter(Mandatory=$true,Position=1)]
-        [securestring]
-        $Password,
+            [Parameter(Mandatory=$true,Position=1)]
+            [securestring]
+            $Password,
 
-        [ValidatePattern('^https?://[\S+]+\.[\S+]+\.[\S]+/PowerShell$', Options='None')]
-        [string]
-        $ServerURI = "http://" + $env:COMPUTERNAME + "." + $env:USERDNSDOMAIN + "/PowerShell",
+            [ValidatePattern('^https?://[\S+]+/PowerShell$', Options='None')]
+            [string]
+            $ServerURI = "http://" + $env:COMPUTERNAME + "." + $env:USERDNSDOMAIN + "/PowerShell",
 
-        # TODO: copy updates to above script params
-        [ValidatePattern('^\S+$')]
-        [string]
-        $Username = $env:USERNAME
-    )
-        $username = "capstone\administrator"
-        $password = ConvertTo-SecureString 'Password1' -AsPlainText -Force
-        $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
-        $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://WIN-90T98I75S6C.capstone.net/PowerShell -Credential $credentials
+            # TODO: copy updates to above script params
+            [ValidatePattern('^\S+$')]
+            [string]
+            $Username = $env:USERNAME
+        )
+
+        $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $Password
+        $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $ServerURI -Credential $credentials
         Import-PSSession $session -AllowClobber
     }
 
+    <#
+        .DESCRIPTION
+        This function adds a send connector which is needed to send mail from the exchange server to the internet.
+
+        .PARAMETER ConnectorDomain
+        Description: Specifies the domain name to which the send connector routes mail.
+        Mantatory: No
+        Default Value: $env:USERDNSDOMAIN
+
+        .PARAMETER ConnectorName
+        Description: Specifies the name of the send connector.
+        Mandatory: No
+        Default Value: "SMTP Mail Send"
+
+        .PARAMETER ConnectorTransportServer
+        Description: Specifies the name of the Mailbox servers that can use this send connector.
+        Mandatory: No
+        Default Value: $env:COMPUTERNAME
+    #>
     function Add-SendConnector {
+        param (
+            [ValidatePattern('^[\S+]+\.[\S]+$')]
+            [string[]]
+            $ConnectorDomain = $env:USERDNSDOMAIN,
+
+            [string]
+            $ConnectorName = "SMTP Mail Send",
+
+            [ValidatePattern('^\S+$')]
+            [string[]]
+            $ConnectorTransportServer = $env:COMPUTERNAME
+        )
+
         # Configure a Send Connector that connects to the internet
-        New-SendConnector -Name "SMTP Mail Send" -Internet -AddressSpaces capstone.net -SourceTransportServers WIN-90T98I75S6C
+        New-SendConnector -Name $ConnectorName -Internet -AddressSpaces $ConnectorDomain -SourceTransportServers $ConnectorTransportServer
+        Write-Output "Created new send connector called $ConnectorName"
     }
 
+    <#
+        .DESCRIPTION
+        This function adds an email address policy. By default it will create a policy where emails follow the following pattern: firstname.lastname@$env:USERDNSDOMAIN
+
+        .PARAMETER EmailPolicyIdentity
+        Description: Specifies the email address policy that you want to modify.
+        Mandatory: No
+        Default Value: Defalt Policy
+
+        .PARAMETER EmailPolicyTemplate
+        Description: Specifies the rules in the email address policy that are used to generate email addresses for recipients.
+        Mandatory: No
+        Defualt Value: "SMTP:%g.%s@" + $env:USERDNSDOMAIN
+    #>
     function Add-EmailAddressPolicy {
-        #Set Default Email Policy
-        Set-EmailAddressPolicy -Identity "Default Policy" -EnabledEmailAddressTemplates "SMTP:%g.%s@capstone.net"
-        Update-EmailAddressPolicy -Identity "Default Policy"   
+        param (
+            [string]
+            $EmailPolicyIdentity = "Default Policy",
+
+            [string]
+            $EmailPolicyTemplate = "SMTP:%g.%s@" + $env:USERDNSDOMAIN
+        )
+
+        $existing_policies = Get-EmailAddressPolicy | Select-Object Name
+
+        If ($EmailPolicyIdentity -eq "Default Policy") {
+            Set-EmailAddressPolicy -Identity "Default Policy" -EnabledEmailAddressTemplates $EmailPolicyTemplate
+            Update-EmailAddressPolicy -Identity "Default Policy"
+            Write-Output "Updated the default policy" 
+        } else {
+            If ($existing_policies.Name -contains $EmailPolicyIdentity) {
+                Write-Output "Policy already exists with the following name: $EmailPolicyIdentity"
+                Write-Output "Updating the policy with the values given"
+                Set-EmailAddressPolicy -Identity $EmailPolicyIdentity -EnabledEmailAddressTemplates $EmailPolicyTemplate
+                Update-EmailAddressPolicy -Identity $EmailPolicyIdentity
+            } else {
+                New-EmailAddressPolicy -Name $EmailPolicyIdentity -IncludedRecipients "MailboxUsers" -EnabledEmailAddressTemplates $EmailPolicyTemplate
+                Update-EmailAddressPolicy -Identity $EmailPolicyIdentity
+            }
+        }
     }
 
+    <#
+        .DESCRIPTION
+        This function updates the firewall rules to accomodate exchange.
+    #>
     function Update-FirewallRules {
         New-NetFirewallRule -DisplayName "SMTP UDP In" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 25
         New-NetFirewallRule -DisplayName "SMTP TCP In" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 25
@@ -170,9 +271,26 @@ process {
         New-NetFirewallRule -DisplayName "SMTP TCP Out" -Direction Outbound -Action Allow -Protocol TCP -LocalPort 25
     }
 
+    <#
+        .DESCRIPTION
+        This function adds mailboxes from a specific organizational unit in active directory if the -UseAD flag is set.
+
+        .PARAMETER ADOrganizationalUnit
+        Description: Mailboxes for every user in this organizational unit will be made
+        Mandatory: Yes
+        Default Value: None
+    #>
     function Add-MailboxFromAD {
+        param (
+            [Parameter(Mandatory=$true)]
+            [string]
+            $ADOrganizationalUnit
+        )
+
         $existing_users = Get-Mailbox | Select-Object Name
-        $target_users = Get-ADUser -filter * -SearchBase "ou=Exchange Users,dc=capstone,dc=net" | Select-Object Name, ObjectGUID
+        $dc_company_component = $env:USERDNSDOMAIN.Split(".")[0]
+        $dc_tld_component = $env:USERDNSDOMAIN.Split(".")[1]
+        $target_users = Get-ADUser -filter * -SearchBase "ou=$ADOrganizationalUnit,dc=$dc_company_component,dc=$dc_tld_component" | Select-Object Name, ObjectGUID
         foreach ($user in $target_users){
             If (-Not($existing_users.Name -contains $user.Name)) {
                 $ObjectGUID = $user.ObjectGUID.ToString()
@@ -214,24 +332,24 @@ process {
     # MAIN
     ########################################
 
-    Write-Output "Username: $Username"
-    Write-Output "Password: $Password"
-    Write-Output "ServerURI: $ServerURI"
-    Write-Output "ConnectorName: $ConnectorName"
-    Write-Output "ConnectorDomain: $ConnectorDomain"
-    Write-Output "ConnectorTransportServer: $ConnectorTransportServer"
-    Write-Output "EmailPolicyIdentity: $EmailPolicyIdentity"
-    Write-Output "EmailPolicyTemplate: $EmailPolicyTemplate"
-    Write-Output "PathToCSV: $PathToCSV"
-    Write-Output "UseAD: $UseAD"
-    Write-Output "ADOrganizationalUnit: $ADOrganizationalUnit"
+    # Write-Output "Username: $Username"
+    # Write-Output "Password: $Password"
+    # Write-Output "ServerURI: $ServerURI"
+    # Write-Output "ConnectorName: $ConnectorName"
+    # Write-Output "ConnectorDomain: $ConnectorDomain"
+    # Write-Output "ConnectorTransportServer: $ConnectorTransportServer"
+    # Write-Output "EmailPolicyIdentity: $EmailPolicyIdentity"
+    # Write-Output "EmailPolicyTemplate: $EmailPolicyTemplate"
+    # Write-Output "PathToCSV: $PathToCSV"
+    # Write-Output "UseAD: $UseAD"
+    # Write-Output "ADOrganizationalUnit: $ADOrganizationalUnit"
 
 
-    # Connect-ExchangeServer
+    # Connect-ExchangeServer -Password $Password -Username $Username -ServerURI $ServerURI
 
-    # Add-SendConnector
+    # Add-SendConnector -ConnectorName $ConnectorName -ConnectorDomain $ConnectorDomain -ConnectorTransportServer $ConnectorTransportServer 
 
-    # Add-EmailAddressPolicy
+    Add-EmailAddressPolicy -EmailPolicyIdentity $EmailPolicyIdentity -EmailPolicyTemplate $EmailPolicyTemplate
 
     # Update-FirewallRules
 
